87f93ff8927243dac3a266490c6dea637d137055
anuragking1995 <anuragshekhawat1995@gmail.com>
2018-05-14 15:44:50 +0530

    26 27

--- js/script.js
+++ js/script.js
@@ -55,7 +55,11 @@ $(document).ready(function(){
 
 	});
 
-
+	$(".counter").each(function(){
+		var element=$(this);
+		var endVal=parseInt(element.text());
+		element.countup(endVal);
+	})
 
 });
 
