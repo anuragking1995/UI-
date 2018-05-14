a91afaa30a9568d8bb8de1e68768414ee749511d
anuragking1995 <anuragshekhawat1995@gmail.com>
2018-05-14 22:33:20 +0530

    40

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
 	$('#slides').superslides({
-		animation:'fade',
-		play:5000,
-		pagination:false
+		animation: 'fade',
+		play: 5000,
+		pagination: false
 	});
 
-	var typed=new Typed(".typed",{
-		strings:["Sofwtare Engineer.","FullStack Developer","Programmer"],
-		typeSpeed:70,
-		loop:true,
-		startDelay:1000,
-		showCursor:false
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
 	});
-
 
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
-	    items:4,
+	    items: 4,
 	    responsive:{
 	        0:{
 	            items:1
@@ -25,7 +25,7 @@ $(document).ready(function(){
 	            items:2
 	        },
 	        768:{
-	        	items:3
+	            items:3
 	        },
 	        938:{
 	            items:4
@@ -34,41 +34,60 @@ $(document).ready(function(){
 	});
 
 
-	var skillsTopOffset=$(".skillsSection").offset().top;
-	var statsTopOffset=$(".statsSection").offset().top;
-	var countUpFinished=false;
-	$(window).scroll(function(){
-		if(window.pageYOffset>skillsTopOffset-$(window).height()+200){	
+	
+
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
 			$('.chart').easyPieChart({
-	            //your options goes here
-	            easing:'easeInOut',
-	            barColor:'#fff',
-	            trackColor:false,
-	            scaleColor:false,
-	            lineWidth:4,
-	            size:152,
-	            onStep:function(from,to,percent){
-	            	$(this.el).find('.percent').text(Math.round(percent));
-            	}
-        	});
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
 
 
 		}
 
-		if(!countUpFinished && window.pageYOffset>statsTopOffset-$(window).height()+200){
-			$(".counter").each(function(){
-			var element=$(this);
-			var endVal=parseInt(element.text());
-			element.countup(endVal);
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
 			})
-			countUpFinished=true;
+
+			countUpFinished = true;
+
 		}
+
 
 	});
 
+
 	$("[data-fancybox]").fancybox();
 
-	
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	})
+
 
 });
 
@@ -77,4 +96,13 @@ $(document).ready(function(){
 
 
 
-	
+
+
+
+
+
+
+
+
+
+
